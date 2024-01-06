99961c04b932ecc4faeb27d5f5b92c41852854da
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-06 04:51:52 +0000

    Successfully made view templates and used erb tags

--- dice.rb
+++ dice.rb
@@ -25,28 +25,25 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
-  outcome = "You rolled #{die}."
+  @die = rand(1..20)
+  @outcome = "You rolled #{@die}."
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -56,8 +53,19 @@ get("/dice/5/4") do
   forth_die = rand(1..4)
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + forth_die + fifth_die
-  outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, #{forth_die} and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, #{forth_die} and a #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
